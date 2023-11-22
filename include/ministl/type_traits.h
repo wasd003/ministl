2ef29eb4deda54c52e7947bb46a3dea62d271f32
wasd003 <wasd003@sjtu.edu.cn>
2023-11-22 12:32:10 +0800

    feat: is_same

--- include/ministl/type_traits.h
+++ include/ministl/type_traits.h
@@ -6,4 +6,14 @@ namespace ministl
 template<typename...>
 using __void_t = void;
 
+template<typename T, typename U>
+struct is_same {
+    constexpr static bool value = false;
+};
+
+template<typename T>
+struct is_same<T, T> {
+    constexpr static bool value = true;
+};
+
 }
--- test/iterator_traits_test.cxx
+++ test/iterator_traits_test.cxx
@@ -1,14 +1,15 @@
 #include <ministl/test.h>
 #include <ministl/iterator.h>
 #include <ministl/vector.h>
+#include <ministl/type_traits.h>
 
 test_result vector_iterator_test() {
     int score = 0, full_score = 0;
 
     // TODO: replace std::is_same_v with ministl ver.
-    static_assert(std::is_same_v<ministl::iterator_traits<
+    static_assert(ministl::is_same<ministl::iterator_traits<
                   ministl::vector<int>::iterator>::iterator_category,
-                  ministl::random_access_iterator_tag>);
+                  ministl::random_access_iterator_tag>::value);
     score ++ , full_score ++ ;
 
     static_assert(!ministl::has_value_type<ministl::vector<int>::iterator>::value);
